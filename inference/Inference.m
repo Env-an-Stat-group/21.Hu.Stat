075408d3cdc63be85e6c7b27eaee9de46da1d044
envandstat <78003588+envandstat@users.noreply.github.com>
2021-08-09 23:59:59 -0400

    Add files via upload

---
+++ inference/Inference.m
@@ -0,0 +1,299 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Inference.m                                                             %
+%    Read and prepare data, and fit model, .                              %
+%    Use restricted likelihood approach for time and likelihood for space %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+setenv('LD_LIBRARY_PATH', '../Shared/:/opt/crc/g/gsl/2.5/gcc/lib/:/afs/crc.nd.edu/x86_64_linux/r/R/3.6.2/gcc/4.8.5/lib64/R/lib/');
+
+%% Setup
+    % Number of workers
+    parpool(10);
+    
+    % General code
+    addpath('../Shared')
+  
+%% Load data
+    % Create datasubset for training stochastic generator
+    disp('Processing data:');
+    numObs = 5;
+    runVec = 1:numObs;
+    [lon, lat, dataSubset] = ProcessData(runVec, '../../SST/', 'pop.h.SST');
+    save('../../Results/trainingData.mat', 'dataSubset', 'lon', 'lat', 'numObs', '-v7.3');
+
+%% Estimate temporal structure
+    disp('Temporal structure:');
+
+    %% Fit VAR(p)
+        disp('Fitting VAR(p):')
+        pOrder = 6;
+        [Phi, Sig, parMat, optRes] = FitVARpprofile_RLIK(dataSubset, pOrder);
+
+        % Save results
+        save('../../Results/temp_VAR6fit.mat', 'pOrder', 'Phi', 'Sig', 'parMat', 'optRes', '-v7.3');
+        
+    %% Estimate mean structure
+        % Full estimate
+        muFull = mean(dataSubset, 4);
+
+        % Time index
+        tVec = (0:1139)'/1140;
+        mVec = 95*tVec;
+
+        % Seasonal basis
+        %sBasis = [ones(1140, 1), cos(2*pi*mVec), sin(2*pi*mVec), cos(2*2*pi*mVec), sin(2*2*pi*mVec), cos(3*2*pi*mVec), sin(3*2*pi*mVec)];
+        sBasis = zeros(1140, 12);
+        for i = 1:12
+            sBasis(i:12:1140, i) = 1;
+        end
+
+        % Yearly basis
+        yBasis = [ones(1140,1), tVec, tVec.^2, tVec.^3];
+
+        % Full basis
+        Hb = ones(1140, size(sBasis,2)*size(yBasis,2));
+        for i = 1:size(sBasis,2)
+            for j = 1:size(yBasis,2)
+                Hb(:,(i-1)*size(yBasis,2)+j) = sBasis(:,i).*yBasis(:,j);
+            end
+        end
+
+        % Least square estimates
+        dim1 = size(dataSubset, 1);
+        dim2 = size(dataSubset, 2);
+        muTmp = reshape(muFull, [dim1*dim2, 1140])';
+        betaTmp = (Hb'*Hb)\(Hb'*muTmp);
+        betaAll = reshape(betaTmp', [dim1 dim2 size(Hb,2)]);
+        muLS = reshape((Hb*betaTmp)', [dim1 dim2, 1140]);
+
+        % Proper estimates
+        betaProp = zeros(dim1, dim2, size(Hb,2));
+        parfor i = 1:dim1
+            for j = 1:dim2
+                if(sum(isnan(parMat(i,j,:))) > 0)
+                    continue;
+                end
+                [~, ~, ~, Qtmp] = ARp_stat_RLIK(parMat(i,j,:), zeros(1140,2), pOrder);
+                betaProp(i,j,:) = (Hb'*Qtmp*Hb)\(Hb'*Qtmp*reshape(muFull(i,j,:), [1140 1]));
+            end
+        end
+        betaTmp = reshape(betaProp, [dim1*dim2, 48])';
+        muWLS = reshape((Hb*betaTmp)', [dim1 dim2, 1140]);
+
+        save('../../Results/temp_mean.mat', 'muLS', 'muWLS', 'betaAll', 'betaTmp');
+                                           
+    %% Prepare spatial residuals for estimation of spatial structure
+        % Detrend
+        disp('De-trending:');
+        dataD = DeTrendP(dataSubset, muWLS, Phi, Sig, pOrder);
+        save('../../Results/deTrendTrainData.mat', 'dataD', 'lon', 'lat', '-v7.3');
+    
+%% Estimate spatial covariance structure
+    %% Calculate mesh and prepare data
+        disp('Making mesh:')
+        tic;
+        % make grid of coordinates
+        %[gLat, gLon] = meshgrid(lat, lon);
+        gLat = lat;
+        gLon = lon;
+
+        % Convert to Cartesian coordinates on unit sphere
+        [x, y, z] = sph2cart(gLon(:)*pi/180, gLat(:)*pi/180, 1);
+        loc = [x y z];
+
+        % Make mesh on sphere
+        [vLoc, tt, tv] = SPDE.MeshGenerator.generateMesh(loc, 'globe', 10, 'maxEdge1', 0.07, 'maxEdge2', 0.07, 'cutOff', 0.07);
+        save('../../Results/mesh.mat', 'vLoc', 'tt', 'tv', 'gLat', 'gLon', 'loc');
+        toc;
+        
+        % Data
+        allObs = reshape(dataD, [dim1*dim2, (1140-pOrder)*5]);
+        monthVV = 6+[(1:(1140-pOrder))', (1:(1140-pOrder))', (1:(1140-pOrder))', (1:(1140-pOrder))', (1:(1140-pOrder))'];
+        monthVV = reshape(monthVV, [(1140-pOrder)*5, 1]);
+        yearVV = repmat((1:95), 12, 1);
+        yearVV = yearVV((pOrder+1):end);
+        yearVV = repmat(yearVV, 1,5);
+        
+        % Permute
+        p = kron((1:(1140-pOrder))', ones([5 1])) + kron(ones([1140-pOrder 1]), (1:(1140-pOrder):4553)'-1);
+        allObs = allObs(:, p);
+        monthVV = monthVV(p);
+        monthVV = 1+ mod(monthVV-1, 12);
+        yearVV = yearVV(p);
+        
+        % Remove unobserved locations
+        nanVec = sum(isnan(allObs), 2);
+        isObs  = nanVec == 0;
+        allObs = allObs(isObs, :);
+        loc = loc(isObs, :);
+        
+        save('../../Results/allObs.mat', 'allObs', 'loc', '-v7.3');
+
+    
+    %% Stationary model
+        %% Fit all data
+             disp('Fitting stationary model. All data:');
+             sTime = tic;
+             
+             % Create SPDE model
+             tWindow = 1:size(allObs,2);
+             tensorParOld = 1;
+             OptStat = SPDE.Optimizer.makeStationaryModel(vLoc, tt, tv, loc, allObs(:, tWindow), 0, tensorParOld);
+
+             % Load starting value
+             x0 = [-5.6; 0; 0; -1; 3.51;2.68];
+
+             % Fit model
+             fitStat = true;
+             if(fitStat)
+                % Set optimization function
+                fun = @(par)(OptStat.logLikelihood(par, [], 1e-4, [], 1, sqrt(eps), 0));
+
+                %% Optimize
+                [xStat, valS] = fminunc(fun, x0, optimset('MaxIter', 2000, 'Display', 'iter-detailed', 'GradObj', 'on', 'LargeScale', 'off'));
+
+                % Store result
+                tFitStat = toc(sTime);
+                save('../../Results/spatial_stat_single.mat', 'xStat', 'tFitStat');
+            else
+                load('../../Results/spatial_stat_single.mat');
+            end
+
+            toc;
+            
+    %% Fit wind-based models
+        %% Three extra parameters
+            disp('Fitting wind model. All data:');
+            sTime = tic;
+
+            % Create SPDE model
+            tWindow = 1:size(allObs,2);
+            tensorParOld = 1;
+            OptWStat = SPDE.Optimizer.makeStationaryModel(vLoc, tt, tv, loc, allObs(:, tWindow), 0, tensorParOld);
+            OptWStat.addWind(1);
+
+            % Load starting value
+            x0 = [xWStat1(1:4); xWStat1(5)*[1; 1; 1];xWStat1(6:7)];
+
+            % Fit model
+            fitWStat = true;
+            if(fitWStat)
+                % Set optimization function
+                fun = @(par)(OptWStat.logLikelihood(par, [], 1e-4, [], 1, sqrt(eps), 0));
+
+                %% Optimize
+                [xWStat3, valWS3] = fminunc(fun, x0, optimset('MaxIter', 2000, 'Display', 'iter-detailed', 'GradObj', 'on', 'LargeScale', 'off'));
+
+                % Store result
+                tFitWStat3 = toc(sTime);
+                save('../../Results/spatial_Wstat3_single.mat', 'xWStat3', 'tFitWStat3');
+            else
+                load('../../Results/spatial_Wstat3_single.mat');
+            end
+
+            toc;
+            
+        %% Spatially varying range scaling 3 par
+            disp('Fitting wind model; more complex. All data:');
+            sTime = tic;
+
+            % Create SPDE model
+            tWindow = 1:size(allObs,2);
+            tensorParOld = 1;
+            OptWNStat = SPDE.Optimizer.makeNonStatModel(vLoc, tt, tv, loc, allObs(:, tWindow), 0, tensorParOld, [4 0 0]);
+            OptWNStat.addWind(1);
+
+            % Load starting value
+            x0 = rand(34, 1)*0.1-0.05;
+%             x0(1:30) = xWNStat1(1:30);
+%             x0(31:32) = xWNStat1(30);
+%             x0(33:34) = xWNStat1(31:32);
+            
+            % Fit model
+            fitWNStat = true;
+            if(fitWNStat)
+                % Set optimization function
+                fun = @(par)(OptWNStat.logLikelihood(par, [], 1e-4, [], 1, sqrt(eps), 0));
+
+                %% Optimize
+                [xWNStat3, valWNS3] = fminunc(fun, x0, optimset('MaxIter', 2000, 'Display', 'iter-detailed', 'GradObj', 'on', 'LargeScale', 'off'));
+
+                % Store result
+                tFitWNStat3 = toc(sTime);
+                save('../../Results/spatial_WNstat3_single.mat', 'xWNStat3', 'tFitWNStat3');
+            else
+                load('../../Results/spatial_WNstat3_single.mat');
+            end
+
+            toc;
+            
+     %% Non-stationary model
+        %% All data
+            disp('Fitting non-stationary model. All data')
+            sTime = tic;
+
+            % Create time window
+            %tWindow = 1:(10*12*5);
+            tWindow = 1:size(allObs,2);
+            
+            % Create SPDE model
+            OptNStat = SPDE.Optimizer.makeNonStatModel(vLoc, tt, tv, loc, allObs(:, tWindow), 0, 1);
+
+            % Start value
+            x0 = rand(78, 1)*0.1-0.05;
+            x0(1) = xStat(1)+xStat(4)/0.28;
+            x0(2) = xStat(1);
+            x0(end-1) = xStat(end-1);
+            x0(end) = xStat(end);
+            
+            % Iterate through windows
+            fitNStat = true;
+            if(fitNStat)
+                % Set OptStatimization function
+                fun = @(par)(OptNStat.logLikelihood(par, [], 1e-4, [], 1, sqrt(eps), 0));
+
+                %% Optimize
+                [xNStat, valNS] = fminunc(fun, x0, optimset('MaxIter', 600, 'Display', 'iter-detailed', 'GradObj', 'on', 'LargeScale', 'off', 'TolFun', 1e-5));
+
+                tFitNStat = toc(sTime);
+                save('../../Results/spatial_nstat_single.mat', 'xNStat', 'tFitNStat');
+            else
+                load('../../Results/spatial_nstat_single.mat');
+            end
+            toc(sTime);
+
+        %% Add neural net wind
+            %% Spatially varying range harmonics and Neural Network + harmonics
+                disp('Fitting NN model');
+                disp('Load estimators from Non-stat model as starting points')
+                load('../../Results/spatial_nstat_single.mat','xNStat')
+                sTime = tic;
+
+                % Create SPDE model
+                tWindow = 1:size(allObs,2);
+                tensorParOld = 1;
+                OptNN = SPDE.Optimizer.makeNN(vLoc, tt, tv, loc, allObs(:, tWindow), 0, tensorParOld, [4 0 4]);
+                nbNum=4;
+                nodeNum=3;
+                OptNN.addWindNN(nbNum, nodeNum);
+                % Load starting value
+                size = 76 + ((nbNum + 1) * nodeNum + 2 * nodeNum + 1)*3 + 2;
+                x0 = rand(size, 1)*0.1-0.05;
+                x0(1:76) = xNStat(1:76);
+                x0((size-1):size) = xNStat(77:78);
+
+                % Iterate through windows
+                fitNN = true;
+                if(fitNN)
+                    fun = @(par)(OptNN.logLikelihood(par, [], 1e-4, [], 1, sqrt(eps), 0));
+
+                    %% Optimize
+                    [xNN, valNN] = fminunc(fun, x0, optimset('MaxIter', 200, 'Display', 'iter-detailed', 'GradObj', 'off', 'LargeScale', 'off'));
+
+                    % Store result
+                    tFitWNStat2 = toc(sTime);
+                    save('../../Results/spatial_NNhm.mat', 'xNN','valNN','tFitWNStat2');
+                else
+                    load('../../Results/spatial_NNhm.mat', 'xNN','valNN','tFitWNStat2');
+                end
+                toc(sTime)
